353c5eeed05465cd2a89282a752329d16bf514df
Niojen <d.horiachok@ukma.edu.ua>
2024-03-22 00:05:30 +0200

    Update asm code

--- assembly/asmcode.asm
+++ assembly/asmcode.asm
@@ -1,5 +1,11 @@
+.model small
+
+.data
+MESSAGE DB "HI, ENTER A NUMBER: $"
+MESSAGE1 DB 10, 13, "The NUMBER is $"
 .model tiny
 .code
+.startup
 org 100h
 
 init:
@@ -23,13 +29,21 @@ start proc
     mov bl, [inputString+1]     
     mov [inputString+2+bx], al
 
+mov ah, 09h
+mov dx, offset MESSAGE
+int 21h
     ; Ініціалізація показників для маніпулювання рядками
     lea si, [inputString + 2] 
     lea di, [outputString + 2]
 
+mov ah, 01h
+int 21h
     ; Ініціалізація лічильника циклів
     mov cx, 0
 
+mov ah, 09h
+mov dx, offset MESSAGE1
+int 21h
 replace:
     ; Завантажує символ із вхідного рядка
     mov al, [si]
@@ -37,12 +51,15 @@ replace:
     cmp al, '$'
     je end_loop
 
+mov dl, al
     ; Перевіряє, чи є символ ">"
     cmp al, '>'
     jne not_replace
     ; Змінює '>' на '+'
     mov al, '+'
 
+mov ah, 02h
+int 21h
 not_replace:
     ; Зберігає символ у вихідному рядку
     mov [di], al
@@ -53,6 +70,8 @@ not_replace:
     ; Повтор
     jmp replace
 
+mov ah, 4ch
+int 21h
 end_loop:
     mov byte ptr [di], '$'
 
@@ -81,4 +100,6 @@ promptMessage db "Enter a string: $"
 outputString db 20 dup(?)
 inputString db 20, 20 dup(?)
 
+end
+.exit
 end init 
